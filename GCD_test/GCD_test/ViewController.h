0719e658f858e1b26ea907ca7a4b5b3f48ca60c2
GT mac <gtmac@GT.local>
2015-12-08 11:07:57 +0800

    no message

---
+++ GCD_test/GCD_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD_test
+//
+//  Created by GT mac on 15/12/8.
+//  Copyright © 2015年 GT mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD_test/GCD_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GCD_test
+//
+//  Created by GT mac on 15/12/8.
+//  Copyright © 2015年 GT mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
